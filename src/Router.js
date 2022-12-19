308f652286bde742e5cbdf7c68a64b80d0f3ffe4
vanc4a <34973234+vanc4a@users.noreply.github.com>
2022-12-20 00:53:34 +0300

    reafctor

--- src/Router.js
+++ src/Router.js
@@ -6,28 +6,28 @@ module.exports = Router = (route) => {
         return process.exit(1)  
     }
     if(args[0] == 'os'){
-        controller.os(route)
+        return controller.os(route)
     }
     if(args[0] == 'cat' && args.length >= controller.cat.length){
-        controller.cat(args[1])
+        return controller.cat(args[1])
     }
     if(args[0] == 'add' && args.length >= controller.add.length){
-        controller.add(args[1])
+        return controller.add(args[1])
     }
     if(args[0] == 'rn' && args.length >= controller.rn.length){
-        controller.rn(args[1],args[2])
+        return controller.rn(args[1],args[2])
     }
     if(args[0] ==  'cp' && args.length >= controller.cp.length){
-        controller.cp(args[1],args[2])
+        return controller.cp(args[1],args[2])
     }
     if(args[0] ==  'rm' && args.length >= controller.rm.length){
-        controller.rm(args[1])
+        return controller.rm(args[1])
     }
     if(args[0] == 'mv' && args.length >= controller.mv.length){
-        controller.mv(args[1],args[2])
+        return controller.mv(args[1],args[2])
     }
     if(args[0] == 'rn' && args.length >= controller.rn.length){
-        controller.rn(args[1],args[2])
+        return controller.rn(args[1],args[2])
     }
     if(args[0] == 'cd' && args.length >= controller.cd.length){
         return controller.cd(args[1])
@@ -35,5 +35,5 @@ module.exports = Router = (route) => {
     if(route == 'ls'){
         return controller.fileList()
     }
-    return 'Undefined command'
+    return console.log('Undefined command')
 }
